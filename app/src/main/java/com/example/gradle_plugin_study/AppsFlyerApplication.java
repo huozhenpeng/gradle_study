3c82f11f0823f4ecba01b398feb0a8a5c9646a0f
霍振鹏 <huozhenpengdyx@gmail.com>
2020-12-09 17:30:17 +0800

    appsflyer

---
+++ app/src/main/java/com/example/gradle_plugin_study/AppsFlyerApplication.java
@@ -0,0 +1,55 @@
+package com.example.gradle_plugin_study;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.appsflyer.AppsFlyerConversionListener;
+import com.appsflyer.AppsFlyerLib;
+
+import java.util.Map;
+
+/**
+ * Date: 2020/11/25
+ * Author: huozhenpeng
+ */
+public class AppsFlyerApplication extends Application {
+    private final String AF_DEV_KEY = "XeLt8cis2ue5dkgN7dw9bL";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppsFlyerConversionListener conversionListener = new AppsFlyerConversionListener() {
+
+
+            @Override
+            public void onConversionDataSuccess(Map<String, Object> conversionData) {
+
+                for (String attrName : conversionData.keySet()) {
+                    Log.d("LOG_TAG", "attribute: " + attrName + " = " + conversionData.get(attrName));
+                }
+            }
+
+            @Override
+            public void onConversionDataFail(String errorMessage) {
+                Log.d("LOG_TAG", "error getting conversion data: " + errorMessage);
+            }
+
+            @Override
+            public void onAppOpenAttribution(Map<String, String> conversionData) {
+
+                for (String attrName : conversionData.keySet()) {
+                    Log.d("LOG_TAG", "attribute: " + attrName + " = " + conversionData.get(attrName));
+                }
+
+            }
+
+            @Override
+            public void onAttributionFailure(String errorMessage) {
+                Log.d("LOG_TAG", "error onAttributionFailure : " + errorMessage);
+            }
+        };
+
+        AppsFlyerLib.getInstance().init(AF_DEV_KEY, conversionListener, getApplicationContext());
+//        AppsFlyerLib.getInstance().setDebugLog(true);
+        AppsFlyerLib.getInstance().start(this);
+    }
+}
